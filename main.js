2394fe276950fd17c9a39a5a3a0a7391a1eb7e4a
KeithTheDev <kgoodrich44@gmail.com>
2018-07-27 23:06:51 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+function arithmetic(a, b, math){
+  if(math === "add"){
+    return a + b;
+  } else if(math === "subtract"){
+    return a - b;
+  } else if(math === "multiply"){
+    return a * b;
+  } else if(math === "divide"){
+    return a / b;
+  }
+}
